221d1bb0fc5026500607a416042eadc8e4a3b531
d3570ns <nodestonk94@gmail.com>
2023-02-20 13:10:28 +0700

    Latihan

---
+++ src/main/java/com/juaracoding/knhsspringbootjpa/repo/ProvinsiRepo.java
@@ -0,0 +1,16 @@
+package com.juaracoding.knhsspringbootjpa.repo;
+/*
+Created By IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author User a.k.a. Kevin Nodeston Hasudungan Silalahi
+Java Developer
+Created on 20/02/2023 12:51
+@Last Modified 20/02/2023 12:51
+Version 1.0
+*/
+
+import com.juaracoding.knhsspringbootjpa.model.Provinsi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvinsiRepo extends JpaRepository<Provinsi,Long> {
+}
